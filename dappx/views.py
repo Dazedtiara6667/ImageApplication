ca2a49db1ab7188acebf0bc4cbafa5146e19bef1
Rishab Mudliar <53506835+Dazedtiara6667@users.noreply.github.com>
2020-03-08 15:11:37 +0530

    Update views.py

--- dappx/views.py
+++ dappx/views.py
@@ -42,7 +42,6 @@ def gallery(request):
 class AlbumDetail(DetailView):
      model = Album
      def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super(AlbumDetail, self).get_context_data(**kwargs)
         # Add in a QuerySet of all the images
         context['images'] = AlbumImage.objects.filter(album=self.object.id)
